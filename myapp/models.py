ab6ebce410de72061ceaa8ac13c674227c01edf0
Matias Piuma <piumamatias@gmail.com>
2023-11-22 11:33:37 -0300

    modificando la base de datos

--- myapp/models.py
+++ myapp/models.py
@@ -4,17 +4,27 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length = 100)
-    description = models.TextField(blank=True)
-    users = models.ForeignKey(User, on_delete = models.DO_NOTHING)
+    description = models.TextField(blank = True)
+
+    def __str__(self):
+        return self.title
+
+class Priority(models.Model):
+    prio = models.CharField(max_length = 25)
+
+    def __str__(self):
+        return self.prio
 
 class Task(models.Model):
     title = models.CharField(max_length = 100)
-    description = models.TextField(blank=True)
-    created = models.DateTimeField(auto_now_add=True)
-    datecompleted = models.DateTimeField(null=True)
-    important = models.BooleanField(default=False)
-    users = models.ForeignKey(User, on_delete = models.DO_NOTHING)
-    project = models.ForeignKey(Project, on_delete = models.CASCADE)
+    description = models.TextField(blank = True)
+    created = models.DateTimeField(auto_now_add = True)
+    datecompleted = models.DateTimeField(null = True)
+    user = models.ForeignKey(User, on_delete = models.DO_NOTHING)
+    project = models.ForeignKey(Project, on_delete = models.CASCADE, default = None)
+    priority = models.ForeignKey(Priority, on_delete = models.DO_NOTHING, default = None)
+    priority = models.ForeignKey(Priority, on_delete = models.DO_NOTHING, default = None)
 
-    def __str__(self) -> str:
+
+    def __str__(self):
         return self.title + ' - by ' + self.user.username
